080f512fd7a5313eea81cfeb38c12660966f263c
Saad <mohammadsaad_b210488cs@nitc.ac.in>
2024-10-29 09:58:25 +0000

    stage 10

--- mynitcbase/BlockAccess/BlockAccess.cpp
+++ mynitcbase/BlockAccess/BlockAccess.cpp
@@ -403,30 +403,30 @@ NOTE: This function will copy the result of the search to the `record` argument.
       The caller should ensure that space is allocated for `record` array
       based on the number of attributes in the relation.
 */
-int BlockAccess::search(int relId, Attribute *record, char attrName[ATTR_SIZE], Attribute attrVal, int op) {
-    // Declare a variable called recid to store the searched record
-    RecId recId;
+// int BlockAccess::search(int relId, Attribute *record, char attrName[ATTR_SIZE], Attribute attrVal, int op) {
+//     // Declare a variable called recid to store the searched record
+//     RecId recId;
 
-    /* search for the record id (recid) corresponding to the attribute with
-    attribute name attrName, with value attrval and satisfying the condition op
-    using linearSearch() */
-    recId = BlockAccess::linearSearch(relId, attrName, attrVal, op);
+//     /* search for the record id (recid) corresponding to the attribute with
+//     attribute name attrName, with value attrval and satisfying the condition op
+//     using linearSearch() */
+//     recId = BlockAccess::linearSearch(relId, attrName, attrVal, op);
 
-    // if there's no record satisfying the given condition (recId = {-1, -1})
-    //    return E_NOTFOUND;
-    if (recId.block == -1 && recId.slot == -1)
-        return E_NOTFOUND;
+//     // if there's no record satisfying the given condition (recId = {-1, -1})
+//     //    return E_NOTFOUND;
+//     if (recId.block == -1 && recId.slot == -1)
+//         return E_NOTFOUND;
 
-    /* Copy the record with record id (recId) to the record buffer (record)
-       For this Instantiate a RecBuffer class object using recId and
-       call the appropriate method to fetch the record
-    */
-    RecBuffer recBuffer(recId.block);
-    recBuffer.getRecord(record, recId.slot);
+//     /* Copy the record with record id (recId) to the record buffer (record)
+//        For this Instantiate a RecBuffer class object using recId and
+//        call the appropriate method to fetch the record
+//     */
+//     RecBuffer recBuffer(recId.block);
+//     recBuffer.getRecord(record, recId.slot);
 
 
-    return SUCCESS;
-}
+//     return SUCCESS;
+// }
 
 int BlockAccess::deleteRelation(char relName[ATTR_SIZE]) {
     // if the relation to delete is either Relation Catalog or Attribute Catalog,
@@ -752,6 +752,59 @@ int BlockAccess::project(int relId, Attribute *record) {
     */
     RecBuffer targetBlock(block);
     targetBlock.getRecord(record, slot);
+
+    return SUCCESS;
+}
+
+int BlockAccess::search(int relId, Attribute *record, char attrName[ATTR_SIZE], Attribute attrVal, int op) {
+    // Declare a variable called recid to store the searched record
+    RecId recId;
+
+    /* get the attribute catalog entry from the attribute cache corresponding
+    to the relation with Id=relid and with attribute_name=attrName  */
+    AttrCatEntry attrCatBuf;
+    int ret = AttrCacheTable::getAttrCatEntry(relId, attrName, &attrCatBuf);
+
+    // if this call returns an error, return the appropriate error code
+    if (ret != SUCCESS)
+        return ret;
+
+    // get rootBlock from the attribute catalog entry
+    int rootBlock = attrCatBuf.rootBlock;
+    /* if Index does not exist for the attribute (check rootBlock == -1) */
+    if (rootBlock == -1) {
+
+        /* search for the record id (recid) corresponding to the attribute with
+           attribute name attrName, with value attrval and satisfying the
+           condition op using linearSearch()
+        */
+        recId = BlockAccess::linearSearch(relId, attrName, attrVal, op);
+    }
+
+    /* else */ 
+    else {
+        // (index exists for the attribute)
+
+
+        /* search for the record id (recid) correspoding to the attribute with
+        attribute name attrName and with value attrval and satisfying the
+        condition op using BPlusTree::bPlusSearch() */
+        recId = BPlusTree::bPlusSearch(relId, attrName, attrVal, op);
+    }
+
+
+    // if there's no record satisfying the given condition (recId = {-1, -1})
+    //     return E_NOTFOUND;
+    if (recId.block == -1 && recId.slot == -1)
+        return E_NOTFOUND;
+
+    /* Copy the record with record id (recId) to the record buffer (record).
+       For this, instantiate a RecBuffer class object by passing the recId and
+       call the appropriate method to fetch the record
+    */
+    RecBuffer recBuffer(recId.block);
+    recBuffer.getRecord(record, recId.slot);
+
 
     return SUCCESS;
 }
